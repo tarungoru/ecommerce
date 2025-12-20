601f24b2637107e23ff744c778953c047f8c7fbc
tarungoru <gorutarunnagasai@gmail.com>
2025-12-20 22:32:11 +0530

    product-view page is done

--- Frontend/src/app/components/product-details/product-details.component.ts
+++ Frontend/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailsComponent {
 
+  activeWeight: string = '250g';
+  quantity: number = 1;
+  reviews = [1,2,4,5,4,6];
+
+  setActiveWeight(weight: string): void {
+    this.activeWeight = weight;
+  }
+
+  incrementQuantity(): void {
+    this.quantity += 1;
+  }
+
+  decrementQuantity(): void {
+    if (this.quantity > 1) {
+      this.quantity -= 1;
+    }
+  }
 }
